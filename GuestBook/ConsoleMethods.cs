cd437d644cd5302f1c15368b67501fd37ab3cd9c
chrisic3 <chris_ic3@hotmail.com>
2025-08-24 19:05:40 -0500

    Add methods class

---
+++ GuestBook/ConsoleMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook
+{
+    internal class ConsoleMethods
+    {
+        /*
+         *  GetGuestName(list)
+         *      ask for name: {name}
+         *      add to list
+         *      
+         *  GetNumberInParty(total)
+         *      ask for number: {number}
+         *      convert to int and add to {number}
+         *      
+         *      
+         *  PrintGuestList(list)
+         *      foreach {name} in {list}
+         *          print {name}
+         */
+    }
+}
--- GuestBook/Program.cs
+++ GuestBook/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿/*  
+ *  Ask the user for their name and how many are in their party. 
+ *  Keep track of how many people are at the party. At the end, print out 
+ *  the guest list and the total number of guests.
+ */
+
+/*  
+ *  var for list: {guestList}
+ *  var for total: {totalGuests}
+ *  var for loop: {isDone}
+ *  
+ *  do
+ *      ask for name: {GetGuestName}
+ *      
+ *      ask for number in party: {GetNumberInParty}
+ *      
+ *      ask if done: {isDone}
+ *  while not {isDone}
+ *  
+ *  {PrintGuestList}
+ *  "There are {totalGuests} total guests."
+ */
+
